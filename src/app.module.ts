66e4ea3908c3d1b38b9c77317a137c387ab483fe
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-09 00:15:12 +0600

    env config and mongodb config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './mongoose/mongoose-config.service';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            useClass: MongooseConfigService,
+        }),
+    ],
 })
 export class AppModule {}
