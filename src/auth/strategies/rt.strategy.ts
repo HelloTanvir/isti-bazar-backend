7dc9867be516e806d9960ee562871f91fddf59c5
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-10 21:09:14 +0600

    getting secret key from env config vars

--- src/auth/strategies/rt.strategy.ts
+++ src/auth/strategies/rt.strategy.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-    constructor() {
+    constructor(private config: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'rt-secret',
+            secretOrKey: config.get('RT_SECRET_KEY'),
             passReqToCallback: true,
         });
     }
