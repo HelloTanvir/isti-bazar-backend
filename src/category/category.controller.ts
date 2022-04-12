250a5f459af74e053efaad0dc882cace78a03f81
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-12 22:07:43 +0600

    route for creating category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { CategoryDto } from './dto';
+import { Category } from './schema';
 
 @Controller('categories')
 export class CategoryController {
     constructor(private categoryService: CategoryService) {}
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() dto: CategoryDto): Promise<Category> {
+        return this.categoryService.create(dto);
+    }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CategoryDto } from './dto';
 import { Category, CategoryDocument } from './schema';
 
 @Injectable()
@@ -8,4 +9,12 @@ export class CategoryService {
     constructor(
         @InjectModel(Category.name) private readonly categoryModel: Model<CategoryDocument>
     ) {}
+
+    async create(dto: CategoryDto): Promise<Category> {
+        const category = new this.categoryModel(dto);
+
+        await category.save();
+
+        return category;
+    }
 }
