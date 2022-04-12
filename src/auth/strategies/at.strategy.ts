05d51d20dca4492401ec943b136c971ca697b75d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-12 17:46:35 +0600

    deactivate access token if the user is logged out

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
 import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User, UserDocument } from '../schema';
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(private config: ConfigService) {
+    constructor(
+        private config: ConfigService,
+        @InjectModel(User.name) private readonly userModel: Model<UserDocument>
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('AT_SECRET_KEY'),
         });
     }
 
-    validate(payload: any) {
+    async validate(payload: any) {
+        const user = await this.userModel.findById(payload['userId']);
+
+        if (!user || !user.refreshToken) {
+            throw new ForbiddenException('invalid user');
+        }
+
         return payload;
     }
 }
