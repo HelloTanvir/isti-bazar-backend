b55082b489cb56444828689702d4c5ecc9cdca38
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-12 21:57:38 +0600

    category module, controller and service with mongodb model

---
+++ src/category/category.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category, CategoryDocument } from './schema';
+
+@Injectable()
+export class CategoryService {
+    constructor(
+        @InjectModel(Category.name) private readonly categoryModel: Model<CategoryDocument>
+    ) {}
+}
