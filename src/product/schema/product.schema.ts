739b396eebae245427b97b1ab9dbe006bd29b1a4
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-13 22:24:13 +0600

    created product schema and added in module and service

---
+++ src/product/schema/product.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ timestamps: true })
+export class Product {
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product name is required'] })
+    name: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product image is required'] })
+    image: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product code is required'], unique: true })
+    code: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product category is required'] })
+    category: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product description is required'] })
+    description: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product size is required'] })
+    size: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product color is required'] })
+    color: string;
+
+    @Prop({
+        type: SchemaTypes.Number,
+        required: [true, 'Product stock is required'],
+        min: [0, 'Product stock must be greater than 0'],
+    })
+    stock: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
