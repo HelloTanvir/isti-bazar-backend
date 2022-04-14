f1429f931f532e7eae976fbac1c52e84ed3cd1d0
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-14 14:47:58 +0600

    stock is not number, it is numeric string as it comes as form data

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
 
 export class ProductDto {
     @IsNotEmpty()
@@ -26,6 +26,6 @@ export class ProductDto {
     color: string;
 
     @IsNotEmpty()
-    @IsNumber()
+    @IsNumberString()
     stock: number;
 }
