e9782de7628af9975d0db5043f8472ff0e82722d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-14 15:30:18 +0600

    custom exception modified

--- src/product/utils/custom-exception.util.ts
+++ src/product/utils/custom-exception.util.ts
@@ -1,14 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import * as fs from 'fs/promises';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
+        const next = ctx.getNext();
 
         if (request.files) {
             (request.files as Express.Multer.File[]).forEach(async (file: Express.Multer.File) => {
@@ -16,10 +15,6 @@ export class HttpExceptionFilter implements ExceptionFilter {
             });
         }
 
-        response.status(status).json({
-            statusCode: status,
-            message: exception.message,
-            error: exception.name,
-        });
+        next(exception);
     }
 }
