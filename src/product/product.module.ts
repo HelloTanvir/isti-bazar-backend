17336002ad6dcfa2730040061916637f524c5d47
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-14 16:57:19 +0600

    check if category is valid or not

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from '../category/schema';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { Product, ProductSchema } from './schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
+    imports: [
+        MongooseModule.forFeature([
+            { name: Product.name, schema: ProductSchema },
+            { name: Category.name, schema: CategorySchema },
+        ]),
+    ],
     controllers: [ProductController],
     providers: [ProductService],
 })
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,12 +1,16 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Category, CategoryDocument } from '../category/schema';
 import { ProductDto } from './dto';
 import { Product, ProductDocument } from './schema';
 
 @Injectable()
 export class ProductService {
-    constructor(@InjectModel(Product.name) private readonly productModel: Model<ProductDocument>) {}
+    constructor(
+        @InjectModel(Product.name) private readonly productModel: Model<ProductDocument>,
+        @InjectModel(Category.name) private readonly categoryModel: Model<CategoryDocument>
+    ) {}
 
     async create(dto: ProductDto, images: Express.Multer.File[]): Promise<Product> {
         if (!images.length) {
@@ -16,6 +20,11 @@ export class ProductService {
         const product = await this.productModel.findOne({ code: dto.code });
         if (product) {
             throw new ForbiddenException('product code already exists');
+        }
+
+        const category = await this.categoryModel.findOne({ name: dto.category });
+        if (!category) {
+            throw new ForbiddenException('category does not exist');
         }
 
         const imagePaths = images.map((image) => image.path);
