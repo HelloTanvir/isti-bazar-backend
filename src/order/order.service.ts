eef07cabcce6c03d83733a0609de88d3276018f2
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-18 13:34:52 +0600

    order schema created

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from '../product/schema';
+import { Order, OrderDocument } from './schema';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(
+        @InjectModel(Order.name) private readonly orderModel: Model<OrderDocument>,
+        @InjectModel(Product.name) private readonly productModel: Model<ProductDocument>
+    ) {}
+}
---
+++ src/order/schema/order.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes } from 'mongoose';
+
+export type OrderDocument = Order & Document;
+
+class ProductInfo {
+    @Prop({ type: SchemaTypes.String, required: [true, 'Product id is required'] })
+    id: string;
+
+    @Prop({
+        type: SchemaTypes.Number,
+        required: [true, 'Product quantity is required'],
+        min: [1, 'Product quantity must be greater than 0'],
+    })
+    quantity: string;
+}
+
+@Schema({ timestamps: true })
+export class Order {
+    // customer details
+    @Prop({ type: SchemaTypes.String, required: [true, 'Customer name is required'] })
+    name: string;
+
+    @Prop({ type: SchemaTypes.String, required: [true, 'Customer phone number is required'] })
+    number: string;
+
+    // product details
+    @Prop({ type: [typeof ProductInfo], required: [true, 'Product details are required'] })
+    products: ProductInfo[];
+
+    // delivery details
+    @Prop({ type: SchemaTypes.String, required: [true, 'Customer address is required'] })
+    address: string;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
