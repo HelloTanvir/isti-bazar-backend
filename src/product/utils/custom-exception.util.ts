62135e32be6a87541a196187791ceca3dff8e42c
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-23 02:48:19 +0600

    image deletion is done in the service, removed from exception handler

--- src/product/utils/custom-exception.util.ts
+++ src/product/utils/custom-exception.util.ts
@@ -1,20 +1,10 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
-import { Request } from 'express';
-import { deleteFile } from './delete-file.util';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
-        const request = ctx.getRequest<Request>();
         const next = ctx.getNext();
-
-        // delete files if body validation fails
-        if (request.files) {
-            (request.files as Express.Multer.File[]).forEach(async (file: Express.Multer.File) => {
-                await deleteFile(file.path);
-            });
-        }
 
         next(exception);
     }
