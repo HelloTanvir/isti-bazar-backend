defdaf676eeab4790a5cc78f9104deb931880a30
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-27 18:00:14 +0600

    variant details updated on product schema

--- src/product/schema/product.schema.ts
+++ src/product/schema/product.schema.ts
@@ -3,6 +3,27 @@ import { Document, SchemaTypes } from 'mongoose';
 
 export type ProductDocument = Product & Document;
 
+class Variants {
+    @Prop({
+        type: SchemaTypes.String,
+        required: [true, 'Variant size is required'],
+    })
+    size: string;
+
+    @Prop({
+        type: SchemaTypes.String,
+        required: [true, 'Variant color is required'],
+    })
+    color: string;
+
+    @Prop({
+        type: SchemaTypes.Number,
+        required: [true, 'Variant stock is required'],
+        min: [0, 'Variant stock must be greater than 0'],
+    })
+    stock: number;
+}
+
 @Schema({ timestamps: true })
 export class Product {
     @Prop({ type: SchemaTypes.String, required: [true, 'Product name is required'] })
@@ -22,10 +43,17 @@ export class Product {
 
     @Prop({
         type: SchemaTypes.Number,
-        required: [true, 'Product price is required'],
-        min: [0, 'Product price must be greater than 0'],
+        required: [true, 'Product buying price is required'],
+        min: [0, 'Product buying price must be greater than 0'],
     })
-    price: number;
+    buyingPrice: number;
+
+    @Prop({
+        type: SchemaTypes.Number,
+        required: [true, 'Product selling price is required'],
+        min: [0, 'Product selling price must be greater than 0'],
+    })
+    sellingPrice: number;
 
     @Prop({
         type: SchemaTypes.Number,
@@ -34,14 +62,12 @@ export class Product {
     })
     stock: number;
 
-    @Prop({ type: SchemaTypes.String, required: [true, 'Product size is required'] })
-    size: string;
-
-    @Prop({ type: SchemaTypes.String, required: [true, 'Product color is required'] })
-    color: string;
-
     @Prop({ type: SchemaTypes.String, required: [true, 'Product description is required'] })
     description: string;
+
+    // variant details
+    @Prop({ type: [typeof Variants], required: [true, 'Variant details are required'] })
+    variants: Variants[];
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
