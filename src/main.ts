be558cf04d90ff1323bae294a1108a873503ff6f
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-04-30 15:16:54 +0600

    cors enable for all

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     // app.enableCors({ origin: ['http://localhost:3000', 'https://isti-bazar.vercel.app'] });
-    app.enableCors({ origin: 'http://localhost:3000' });
+    // app.enableCors({ origin: 'http://localhost:3000' });
+    app.enableCors({ origin: true });
     await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
