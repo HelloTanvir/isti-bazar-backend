63b92e05227659fc873a30c38360147364506f3b
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-05-01 17:24:45 +0600

    cors test

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,8 @@ async function bootstrap() {
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-    // app.use((req: Request, res: Response, next: NextFunction) => {
-    //     res.header(
-    //         'Access-Control-Allow-Origin',
-    //         'https://isti-bazar.vercel.app,http://localhost:3000'
-    //     );
-    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    //     next();
-    // });
-
-    // app.enableCors({
-    //     allowedHeaders: '*',
-    //     origin: ['https://isti-bazar.vercel.app', 'http://localhost:3000'],
-    // });
-
-    const whitelist = ['http://localhost:3000', 'https://isti-bazar.vercel.app'];
     app.enableCors({
-        origin: function (origin, callback) {
-            if (!origin || whitelist.indexOf(origin) !== -1) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: '*',
     });
 
     await app.listen(process.env.PORT || 5000);
