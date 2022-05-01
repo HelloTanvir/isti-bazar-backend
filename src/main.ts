073564603b7c79b6a14ab6ef103332d8c1a74bff
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-05-01 18:47:06 +0600

    cors test

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NextFunction, Request, Response } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-    // app.enableCors({
-    //     origin: '*',
-    // });
-
-    app.use((req: Request, res: Response, next: NextFunction) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept'
-        );
-        next();
+    app.enableCors({
+        origin: true,
     });
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
     await app.listen(process.env.PORT || 5000);
 }
