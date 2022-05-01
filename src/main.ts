9166b12bbc1a0a81768da1510f75aeed0082d778
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-05-01 16:03:39 +0600

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NextFunction, Response } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-    app.enableCors({
-        origin: [
-            'http://localhost:3000',
-            'http://127.0.0.1:3000',
-            'https://isti-bazar.vercel.app',
-            '*',
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true,
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        next();
     });
+
+    app.enableCors({
+        allowedHeaders: '*',
+        origin: '*',
+    });
+
     await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
