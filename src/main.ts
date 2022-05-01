2b7f45c8f658a4837a44846d34ebaa60a937c263
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-05-01 23:01:15 +0600

    vercel config and main file updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: true,
+        origin: ['https://isti-bazar.vercel.app', 'http://localhost:3000'],
     });
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
