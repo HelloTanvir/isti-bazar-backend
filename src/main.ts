bc471244d3cc19e51c3a23487c057ddb960d9851
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-05-01 17:13:10 +0600

    cors test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NextFunction, Response } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,15 +7,15 @@ async function bootstrap() {
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-    app.use((req: Request, res: Response, next: NextFunction) => {
-        res.header(
-            'Access-Control-Allow-Origin',
-            'https://isti-bazar.vercel.app,http://localhost:3000'
-        );
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-        next();
-    });
+    // app.use((req: Request, res: Response, next: NextFunction) => {
+    //     res.header(
+    //         'Access-Control-Allow-Origin',
+    //         'https://isti-bazar.vercel.app,http://localhost:3000'
+    //     );
+    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    //     next();
+    // });
 
     // app.enableCors({
     //     allowedHeaders: '*',
