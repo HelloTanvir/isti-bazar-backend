b6953ea435a9a6f2011c08d4fb80dfb89f683e2d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-05-01 16:25:44 +0600

    cors error fixed

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
     app.use((req: Request, res: Response, next: NextFunction) => {
-        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', [
+            'https://isti-bazar.vercel.app',
+            'http://localhost:3000',
+        ]);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
         next();
@@ -17,7 +20,7 @@ async function bootstrap() {
 
     app.enableCors({
         allowedHeaders: '*',
-        origin: '*',
+        origin: ['https://isti-bazar.vercel.app', 'http://localhost:3000'],
     });
 
     await app.listen(process.env.PORT || 5000);
