fe74495990bc41d047f3afe7ecb18eda8057cdb2
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-01 23:21:01 +0600

    CORS allowed sites defined

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'https://isti-bazar.vercel.app',
+            'http://localhost:3000',
+            'http://127.0.0.1:3000',
+            '*',
+        ],
+    });
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
