846fb002dcaa77c1eea754a737d7303666e2603a
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-01 10:42:06 +0600

    variant removed from product update dto as update of variant is handled on a different route

--- src/product/dto/product-update.dto.ts
+++ src/product/dto/product-update.dto.ts
@@ -1,34 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform, Type } from 'class-transformer';
-import {
-    IsArray,
-    IsNotEmpty,
-    IsNumberString,
-    IsOptional,
-    IsString,
-    // eslint-disable-next-line prettier/prettier
-    ValidateNested
-} from 'class-validator';
-
-class Variant {
-    @ApiProperty({ example: 'Small', description: 'Size of the variant' })
-    @IsOptional()
-    @IsNotEmpty()
-    @IsString()
-    size: string;
-
-    @ApiProperty({ example: 'Black', description: 'Color of the variant' })
-    @IsOptional()
-    @IsNotEmpty()
-    @IsString()
-    color: string;
-
-    @ApiProperty({ example: 'Small', description: 'Size of the variant' })
-    @IsOptional()
-    @IsNotEmpty()
-    @IsNumberString({ message: 'Variant stock must be a number' })
-    stock: number;
-}
+import { IsNotEmpty, IsNumberString, IsOptional, IsString } from 'class-validator';
 
 export class ProductUpdateDto {
     @ApiProperty({ example: 'Macbook Air', description: 'Title of the product' })
@@ -66,33 +37,4 @@ export class ProductUpdateDto {
     @IsNotEmpty()
     @IsString()
     description: string;
-
-    @ApiProperty({
-        type: [Variant],
-        isArray: true,
-        example: [
-            {
-                size: 'Small',
-                color: 'Silver',
-                stock: 10,
-            },
-        ],
-    })
-    @IsOptional()
-    @IsNotEmpty()
-    @Transform(
-        ({ value }) => {
-            if (value && typeof value === 'string') {
-                return JSON.parse(value);
-            } else if (value && typeof value === 'object') {
-                return value;
-            }
-            return [];
-        },
-        { toClassOnly: true }
-    )
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => Variant)
-    variants: Variant[];
 }
