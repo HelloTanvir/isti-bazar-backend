17464765eebb75e97536518a33bb8a8280f091d6
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-11-05 20:44:17 +0600

    empty check on filter queries

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -66,7 +66,11 @@ export class OrderService {
     ): Promise<Order[]> {
         Object.keys(filterQuery).forEach((key) => {
             // if any filter query option is empty, remove it from filter query
-            if (filterQuery[key] === '') {
+            if (
+                filterQuery[key] === '' ||
+                filterQuery[key] === null ||
+                filterQuery[key] === undefined
+            ) {
                 delete filterQuery[key];
             } else if (key === 'startDate') {
                 // if filter query option has startDate, convert this to Date object according to mongodb format
